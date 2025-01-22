60b6db7b62e56dbee8359be9f53a27bc8617c76c
Fahad Iqbal <fahadiqbal9318@gmail.com>
2025-01-22 23:10:06 +0500

    Uncomment code of lesson 2.

--- 2. Conditionals & Loops in Python/main.py
+++ 2. Conditionals & Loops in Python/main.py
@@ -1,14 +1,14 @@
 # Step 1: If-Else Statements – Decision Making
 
 # Checking if savings are positive or negative
-# savings = float(input("Enter your monthly savings: "))
+savings = float(input("Enter your monthly savings: "))
 
-# if savings > 0:
-#     print("Great! You are saving money")
-# elif savings == 0:
-#     print("You are breaking even")
-# else:
-#     print("Warning! You are spending more than you earn.")
+if savings > 0:
+    print("Great! You are saving money")
+elif savings == 0:
+    print("You are breaking even")
+else:
+    print("Warning! You are spending more than you earn.")
 
 # Task 1
 # Write a program that asks for a user's income and expenses, then:
@@ -16,32 +16,32 @@
 # Prints "You are breaking even." if income == expenses.
 # Prints "You are in loss!" if income < expenses.
 
-# monthly_income = float(input("Enter your monthly income: "))
-# monthly_expenses = float(input("Enter your monthly expenses: "))
+monthly_income = float(input("Enter your monthly income: "))
+monthly_expenses = float(input("Enter your monthly expenses: "))
 
-# if monthly_income > monthly_expenses:
-#     print("You are making a profit!")
-# elif monthly_income == monthly_expenses:
-#     print("You are breaking even.")
-# else:
-#     print("You are in loss!")
+if monthly_income > monthly_expenses:
+    print("You are making a profit!")
+elif monthly_income == monthly_expenses:
+    print("You are breaking even.")
+else:
+    print("You are in loss!")
 
 # Step 2: Loops – Automating Tasks
 
 # For Loop (Fixed Repetitions)
 # Print monthly savings for 6 months
-# for month in range(1, 7):
-#     print(f"Month {month}: You saved ${200}")
+for month in range(1, 7):
+    print(f"Month {month}: You saved ${200}")
 
 # While Loop (Unknown Repetitions)
 # Count months until savings reach $1000
-# savings = 0
-# months = 0
+savings = 0
+months = 0
 
-# while savings < 1000:
-#     savings += 200          # Saving $200 per month
-#     months += 1
-#     print(f"After {months} months, savings: {savings}")
+while savings < 1000:
+    savings += 200          # Saving $200 per month
+    months += 1
+    print(f"After {months} months, savings: {savings}")
 
 # Task 1
 # Write a program that:
